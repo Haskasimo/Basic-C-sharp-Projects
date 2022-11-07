a2bd30f64bfbc952c7c30a653436981914578871
Haskasimo <marahaggar@gmail.com>
2022-11-07 11:34:44 -0700

    Class assignment
    adding class assignment to use out parameters

---
+++ ClassAssignment1/ClassAssignment1/Divide.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment1
+{
+    class Divide
+    {
+        public static void DivideBy2(int UserChoice)
+        {
+            int divide2 = UserChoice / 2;
+            Console.WriteLine(divide2);
+        }
+
+        public static void OutNum(out int Number1)
+        {
+            Number1 = 30;
+            Number1 += Number1 ;
+            Console.WriteLine("My number doubled is  " +Number1);
+        }
+
+        public static void OutNum(out int Number1, out int Number2)
+        {
+            Number1 = 30;
+            Number1 += Number1;
+
+            Number2 = 25;
+            Number2 += Number2;
+            Console.WriteLine("The first number doubled is {0}" + Number1);
+            Console.WriteLine("The Second number doubled is {0} " + Number2);
+        }
+    }
+}
---
+++ ClassAssignment1/ClassAssignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide DivTwo = new Divide();
+
+            Console.WriteLine("Hello, Please give me a number \n I'm going to divide it by two. ");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            Divide.DivideBy2(UserNumber);
+
+            int Number1;
+            int Number2;
+            Divide.OutNum( out Number1);
+            Divide.OutNum(out Number1, out Number2);
+            Console.ReadLine();
+        }
+    }
+}
