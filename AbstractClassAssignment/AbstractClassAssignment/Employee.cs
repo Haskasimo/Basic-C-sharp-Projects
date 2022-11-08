038b502270e9745b8e33c7db2c9ebdfa13af80d9
Haskasimo <marahaggar@gmail.com>
2022-11-08 00:29:12 -0700

    added abstract assignment
    added console app that practices the use of  abstract methods and classes

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Employee : Person
+    {
+        public override void SayName()//the override method takes the method from its inherited class but changes it to have new paramaters and function. 
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name : {0}", FullName);
+        }
+    }
+}
--- InheritanceProject1/InheritanceProject1/Person.cs
+++ InheritanceProject1/InheritanceProject1/Person.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace InheritanceProject1
 {
-   public abstract class Person// this is a basic class that other methods can esily inherit from
+   public class Person// this is a basic class that other methods can esily inherit from
     {
         //the properties of the class
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         //the behavior of the class. 
-        public abstract void SayName();//this is a abstract method and it will not actually do anything, but it staters that nay class inheriting must have a SayName Method.  
-        
-        
+        public void SayName()//this is a void method and it will not return value, it will store information that can be printed to the console. 
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: {0}", FullName);
+        }
     }
 }
