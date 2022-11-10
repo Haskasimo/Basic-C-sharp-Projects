d7a29a3e77a4cdf3d15ef6977bcafcd12c33b133
Haskasimo <marahaggar@gmail.com>
2022-11-09 17:06:27 -0700

    update
    updating code

--- OperatorOverloading/OperatorOverloading/Program.cs
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace OperatorOverloading
 {
+
+    //This program uses operator overloading. instead of the operator just working in a 
     class Program
     {
         static void Main(string[] args)
         {
+            Employee employee1 = new Employee();//creating object 1 from the Employee class
+            employee1.Id = 1283;
+         
+
+            Employee employee2 = new Employee();//creating object 2 from the employee class
+            employee2.Id = 13405;
+
+            Console.WriteLine(employee1 == employee2);// this compares employee1 to employee but instead of compareing the object names, it triggers the operator overload and runs the compare method in the employee class which targets the Id instead of the name. 
+            Console.ReadLine();
         }
     }
 }
